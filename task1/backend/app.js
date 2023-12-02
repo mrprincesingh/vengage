f14c0ca554c16815d1312de30d0dde5c87a381d8
mrprincesinghhh <princeranu98@gmail.com>
2023-12-02 17:55:05 +0530

    added file

--- task1/backend/app.js
+++ task1/backend/app.js
@@ -1,34 +1,38 @@
 import express from "express";
-import {config} from "dotenv";
+import { config } from "dotenv";
 import cors from "cors";
 import bodyParser from "body-parser";
+import contact from "./routes/contactRoute.js";
+import { ErrorMiddleware } from "./middleware/Error.js";
+
 config({
-    path:"./config/config.env"
-})
+  path: "./config/config.env",
+});
 
 const app = express();
-app.use(express.json())
-app.use(express.urlencoded({
-  extended: true,
-}))
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(
-    cors({
-      origin: process.env.FRONTEND_URL,
-      credentials: true,
-      methods: ["GET", "POST", "PUT", "DELETE"],
-    })
-  );
-
-  import { ErrorMiddleware } from "./middleware/Error.js";
-  import contact from "./routes/contactRoute.js"
-  app.use("/api/v1/" ,contact)
-  export default app ;
-  app.get("/", (req, res) =>
+app.get("/", (req, res) =>
   res.send(
     `<h1>Site is Working. click <a href=${process.env.FRONTEND_URL}>here</a> to visit frontend.</h1>`
   )
 );
 
-app.use(ErrorMiddleware)
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
+app.use("/api/v1/", contact);
+app.use(ErrorMiddleware);
+
+export default app;
